a2d26982cad18de9b96aebdcc9239a6101f06a80
PRAVEEN YADAV <visionwithvibe0987@gmail.com>
2026-09-08 19:46:23 +0530

    add subscription model

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -181,7 +181,7 @@ const registerUser = asyncHandler(async(req,res) =>{
     const refreshAccessToken = asyncHandler(async(req,res) => {
       const incomingRefreshToken = req.cookie.refreshToken || req.body.refreshToken
 
-      if(incomingRefreshToken){
+      if(! incomingRefreshToken){
         throw new ApiError(401, "unuthorised requiest")
       }
 
