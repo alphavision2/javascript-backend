806b2becdd22ade4301d350ea79923d0f3ae2f78
PRAVEEN YADAV <visionwithvibe0987@gmail.com>
2026-09-11 11:02:26 +0530

    add: extend user routes to include account management and watch history endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { loginUser, registerUser,logoutUser,refreshAccessToken } from "../controllers/user.controllers.js";
+import { loginUser, registerUser,logoutUser,refreshAccessToken, getCurrentUser, updateAccountDetail, updateUserAvatar, updateUserCoverImage, getUserChaannelProfile, getWatchHistory } from "../controllers/user.controllers.js";
 import{upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { get } from "mongoose";
 const router = Router()
 
 router.route("/register").post(
@@ -22,5 +23,11 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-Token").post(refreshAccessToken)
-
+router.route("/change-password").post(verifyJWT, chnageCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetail)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+ router.route("/c/:username").get(verifyJWT,getUserChaannelProfile)
+ router.route("/history").get(verifyJWT,get)
 export default router;
